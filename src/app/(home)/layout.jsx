ec39e258d4ff55be62fc086571815fcf11350699
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-15 23:37:43 +0600

    ডাইনামিক এবং মাল্টি-স্টেপ ফর্ম

--- src/app/(home)/layout.jsx
+++ src/app/(home)/layout.jsx
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="bn">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
---
+++ src/app/(home)/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>লাইভ আপডেট বা ইভেন্ট ঘোষণার জন্য সেকশন।</div>
+  )
+}
+
+export default page
