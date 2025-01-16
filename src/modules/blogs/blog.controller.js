e8bf0077826d9961e62cc037f144261e1487fea8
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-16 08:06:37 +0600

    jsconfig.json;;

--- src/modules/blogs/blog.controller.js
+++
@@ -1,9 +0,0 @@
-import { fetchAllBlogs, addNewBlog } from './blog.services';
-
-export const getBlogs = async () => {
-  return await fetchAllBlogs();
-};
-
-export const createBlog = async (blogData) => {
-  return await addNewBlog(blogData);
-};
