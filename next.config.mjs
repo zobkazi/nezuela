8af111922c831ec260582788eab17c3a44d8fcfe
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-22 10:45:49 +0600

    generateStaticParamspp0

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,9 @@
+import { withContentlayer } from "next-contentlayer";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async headers() {
-        return [
-        {
-            source: '/api/:path*',
-            headers: [
-            {
-                key: 'Access-Control-Allow-Origin',
-                value: '*',
-            },
-            {
-                key: 'Access-Control-Allow-Methods',
-                value: 'GET, POST, PUT, DELETE, OPTIONS',
-            },
-            {
-                key: 'Access-Control-Allow-Headers',
-                value: 'X-Requested-With, Content-Type, Authorization',
-            },
-            ],
-        },
-        ];
-    },
-    output: {
-        filename: 'static/js/[name].[contenthash].js',
-        assetModuleFilename: 'static/assets/[hash][ext][query]',
-    },
-    reactStrictMode: true,
-    images: {
-        domains: ['example.com'],
-    },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
