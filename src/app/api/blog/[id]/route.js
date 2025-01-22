c6d2c7e0dbdf57e9c89fa3b1fc72457064232884
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-22 10:50:34 +0600

    generateStaticParamspp1

--- src/app/api/blog/[id]/route.js
+++ src/app/api/blog/[id]/route.js
@@ -1,13 +1,17 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/libs/connectDB';
 import Blog from '@/modules/blog/blog.schema';
+import connectMongo from '@/db/connectMongo';
+import { getSingleProduct } from '@/db/queries/product.queries';
 
+// Function to generate static params for blogs
 export async function generateStaticParams() {
   await connectDB();
   const blogs = await Blog.find({}, '_id');
   return blogs.map(blog => ({ params: { id: blog._id.toString() } }));
 }
 
+// Function to get a blog by ID
 export async function GET(req, { params }) {
   try {
     await connectDB();
@@ -29,3 +33,14 @@ export async function GET(req, { params }) {
     return NextResponse.json({ error: 'Failed to fetch blog', details: error.message }, { status: 500 });
   }
 }
+
+// Function to get a single product by ID
+export const GETProduct = async (req, { params: { id } }) => {
+  await connectMongo();
+  try {
+    const product = await getSingleProduct(id);
+    return NextResponse.json({ stock: product.stock_count }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch product', details: error.message }, { status: 500 });
+  }
+};
