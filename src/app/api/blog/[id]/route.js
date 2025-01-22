9beb5cd30a2a3149b16cdd08c54bc02847718209
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-22 12:17:46 +0600

    build 1110

--- src/app/api/blog/[id]/route.js
+++ src/app/api/blog/[id]/route.js
@@ -1,13 +1,20 @@
 import connectDB from '@/libs/connectDB';
 import Blog from '@/modules/blog/blog.schema';
 
+export async function generateStaticParams() {
+    const blogs = await Blog.find({}); // অথবা আপনি ডাটাবেস থেকে ব্লগ আইডিগুলি নিয়ে আসতে পারেন
+
+    // প্রতিটি ব্লগ আইডির জন্য প্যারামিটার তৈরি
+    return blogs.map((blog) => ({
+        id: blog._id.toString(),
+    }));
+}
+
 export async function GET(req, { params }) {
     await connectDB();
 
     try {
-        // Await params before using it
-        const { id } = await params;  // Awaiting params
-
+        const { id } = params; // Now params is static because of generateStaticParams()
         const blog = await Blog.findById(id);
         if (!blog) {
             return new Response(JSON.stringify({ error: 'Blog not found' }), { status: 404 });
