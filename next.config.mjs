2d30c190aab795e3195826397ca6f12f1d5b7fa2
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-26 22:46:06 +0600

    next-config.mjs file update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    webpack5: true,
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "https://zobkazi.github.io/nezuela/api/:path*",
+            },
+        ];
+    },
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET, POST, PUT, DELETE",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-Requested-With, Content-Type, Accept",
+                    },
+                ],
+            },
+        ];
+    },
+}
 
 export default nextConfig;
