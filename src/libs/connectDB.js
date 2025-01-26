8c143818fbec9a9a35ebc4fc076a513d83dc6da9
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-26 22:17:05 +0600

    jan/26:10:16:29pm

--- src/libs/connectDB.js
+++ src/libs/connectDB.js
@@ -1,14 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+let isConnected = false; // Track the connection state
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable in .env.local');
-}
+const connectToDatabase = async () => {
+  if (isConnected) {
+    console.log("MongoDB already connected!");
+    return;
+  }
 
+  if (!process.env.MONGODB_URI) {
+    console.error("Invalid Config: MONGODB_URI is not defined");
+    throw new Error("MONGODB_URI is not defined");
+  }
 
-async function connectToDatabase() {
-  mongoose.connect(MONGODB_URI)
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+};
 
 export default connectToDatabase;
