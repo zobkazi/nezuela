67be4c32b573ffc706f2c77bb2a484f38928c3b5
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-26 23:25:35 +0600

    next-config.mjs file update-2

--- src/app/(home)/blog/page.jsx
+++ src/app/(home)/blog/page.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+export default async function Page() {
+  const res = await fetch('https://bdtime24-net-api-6lh5.onrender.com/api/news');
+  const responseData = await res.json();
 
-const page = () => {
+  // Access the articles array
+  const articles = responseData.articles || [];
+
+  // Check if articles exist
+  if (articles.length === 0) {
+    return <p>No articles available at the moment.</p>;
+  }
+
   return (
-    <div>page</div>
-  )
-}
+    <ul>
+      {articles.map((article) => (
+        <>
+        <li key={article.id}>{article.title}</li>
+       <li>
+          {article.description}
+       </li>
+        </>
 
-export default page
+      ))}
+    </ul>
+  );
+}
--- src/app/api/blog/get/route.js
+++ src/app/api/blog/get/route.js
@@ -1,33 +1,12 @@
-import Blog from "@/modules/blog/blog.schema";
-import connectToDatabase from "@/libs/connectDB";
-import { NextResponse } from "next/server";
-
-export const GET = async (req) => {
-  try {
-    // Connect to the database
-    await connectToDatabase();
-
-    // Extract query parameters from the request
-    const title = req.nextUrl.searchParams.get("title");
-    const content = req.nextUrl.searchParams.get("content");
-    const category = req.nextUrl.searchParams.get("category");
-
-    // Build the query dynamically
-    const query = {};
-    if (title) query.title = title;
-    if (content) query.content = content;
-    if (category) query.category = category;
-
-    // Fetch blogs from the database with lean for plain objects
-    const blogs = await Blog.find(query).lean();
-
-    // Return the data as a JSON response
-    return NextResponse.json({ success: true, blogs: blogs });
-  } catch (error) {
-    console.error("Error fetching blogs:", error);
-    return NextResponse.json(
-      { success: false, message: "Internal Server Error", details: error.message },
-      { status: 500 }
-    );
-  }
-};
+export const dynamic = 'force-static'
+ 
+export async function GET() {
+  const res = await fetch('https://bdtime24-net-api-6lh5.onrender.com/api/news', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  const data = await res.json()
+ 
+  return Response.json({ data })
+}
