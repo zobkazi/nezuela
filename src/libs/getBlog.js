4dc5c925a3ddcdc202b96554506f0861c3ba6982
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-27 12:58:10 +0600

    not a

--- src/libs/getBlog.js
+++ src/libs/getBlog.js
@@ -1,6 +1,6 @@
-export default async function getPost(id) {
+export default async function getPost(_id) {
     const result = await fetch(
-        `http://localhost:3000/api/blog${id}`,
+        `http://localhost:3000/api/blog${_id}`,
     );
 
     return result.json();
