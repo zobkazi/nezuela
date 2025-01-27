bb6ac1bed6a6cd2c1cb70d9d82ceaaf22512bc17
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-27 07:33:09 +0600

    next-config.mjs file update-4

--- src/app/(home)/blog/[_id]/page.jsx
+++ src/app/(home)/blog/[_id]/page.jsx
@@ -1,36 +1,11 @@
 import React from 'react'
 
-const page = async ({
-  params: {
-    _id
-  }
 
-  
-}) => {
-
-  const apiEndpoint = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
-  const res = await fetch(`${apiEndpoint}/api/news${_id}`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    cache: 'no-cache',
-
-  });
-
-  const responseData = await res.json();
-
-  console.log(responseData.articles)
+export default async function BlogDetailPage({ _id }) {
   return (
     <div>
-      <h1>Page: { _id }</h1>
-
-     <p>Page content</p>
-
-     <h1>
-      
-     </h1>
+      <h1>Blog Detail Page</h1>
+      <p>Blog ID: {_id}</p>
     </div>
   )
 }
-
-export default page
